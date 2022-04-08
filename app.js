359301b7bf9191f2482d6d52ef5feb697dad19b7
alaJream <jeremy.gonzales@students.dominican.edu>
2022-04-07 23:11:03 -0700

    users can create events

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = express()
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyParser = require('body-parser');
+const models = require('./db/models');
 
+const hbs = exphbs.create({
+    defaultLayout: 'main', 
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+  });
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // app.get('/', (req, res) => {
@@ -20,11 +28,26 @@ var events = [
     { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
   
-  // INDEX
+// Index
 app.get('/', (req, res) => {
-    res.render('events-index', { events: events });
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+      res.render('events-index', { events: events });
+    })
+  })
+
+// NEW
+app.get('/events/new', (req, res) => {
+    res.render('events-new', {});
 })
 
+// CREATE
+app.post('/events', (req, res) => {
+    models.Event.create(req.body).then(event => {
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err)
+    });
+})
 
 
 // ---------------------------------------------------------------------------------------------------------
