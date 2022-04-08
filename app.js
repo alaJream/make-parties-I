e98b6c10441488bacb868a99fc09062fa7ec00fd
alaJream <jeremy.gonzales@students.dominican.edu>
2022-04-07 23:28:56 -0700

    users can edit and update events

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
@@ -15,7 +16,7 @@ const hbs = exphbs.create({
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'))
 
 // app.get('/', (req, res) => {
 //     res.render('home', { msg: 'Handlebars are Cool!' });
@@ -43,7 +44,6 @@ app.get('/events/new', (req, res) => {
 // CREATE
 app.post('/events', (req, res) => {
     models.Event.create(req.body).then(event => {
-      // Redirect to events/:id
         res.redirect(`/events/${event.id}`)
     }).catch((err) => {
       console.log(err)
@@ -52,15 +52,34 @@ app.post('/events', (req, res) => {
 
 // SHOW
 app.get('/events/:id', (req, res) => {
-    // Search for the event by its id that was passed in via req.params
     models.Event.findByPk(req.params.id).then((event) => {
-      // If the id is for a valid event, show it
       res.render('events-show', { event: event })
     }).catch((err) => {
-      // if they id was for an event not in our db, log an error
       console.log(err.message);
     })
 })
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+    models.Event.findByPk(req.params.id).then((event) => {
+      res.render('events-edit', { event: event });
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  });
+
+// UPDATE
+app.put('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id).then(event => {
+      event.update(req.body).then(event => {
+        res.redirect(`/events/${req.params.id}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
 
 
 // ---------------------------------------------------------------------------------------------------------
